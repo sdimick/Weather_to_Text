353ef50fcf4dd7e022f112bb8f5e358d297e8c09
sdimick <seth.dimick@gmail.com>
2016-04-15 16:07:22 -0700

    Updated source to diffenet website due to website changes.

--- weathertotext.py
+++ weathertotext.py
@@ -12,9 +12,10 @@ from bs4 import BeautifulSoup
 import time
 
 #Use selenium and the chromedriver to go to navigate to the hourly forecast online
-#then import the html source code as your soup for Beautiful Soup
-driver = webdriver.Chrome('/Path/to/your/chromedriver/chromedriver')
-driver.get("https://m.weather.com/weather/hourbyhour/graph/Seattle+WA+98122:4:US")
+#then import the html source as your soup for Beautiful Soup
+driver = webdriver.Chrome('/Users/mvdimick/Documents/Seth/PythonDev/chromedriver/chromedriver')
+driver.get("https://weather.weatherbug.com/forecasts/hourly/seattle-wa-98122")
+driver.refresh()
 html = driver.page_source
 driver.quit()
 soup = BeautifulSoup(html, 'html.parser')
@@ -24,36 +25,30 @@ hours = []
 temps = []
 newtemps = []
 precip = []
-newprecip = []
 
 #Use Beautiful soup to scrape the html for our weather info and store it in our lists
-for element in soup('li', {'class' : 'mw-hourly-time'}):
+for element in soup('span', {'class' : 'hour'}):
     hours.append(str(element.contents[0]))
 
-for element in soup('li', {'class' : 'mw-hourly-temp'}):
+for element in soup('div', {'class' : 'temp'}):
     temps.append(element.contents[0])
 
 for x in temps:
     newtemps.append(str(x.replace(u'\xa0', ' ').encode('utf-8')))
 
-for element in soup('li', {'class' : 'mw-hourly-precip'}):
-    precip.append(str(''.join(element.find('br').next_siblings)))
+for element in soup('div', {'class' : 'precip'}):
+    precip.append(str(''.join(element.find('span'))))
 
-for x in precip:
-    newprecip.append(x.strip())
 
 #From our lists of data, create strings that will be sent as text messages
 forecast = "HOUR TEMP PRECIP"
 for i in range(6):
-    forecast = forecast + "\n" + " " + hours[i] + "   " + newtemps[i] + "    " + newprecip[i]
+    forecast = forecast + "\n" + " " + hours[i] + "   " + newtemps[i]  + "   " + precip[i]
 
 forecast2 = "HOUR TEMP PRECIP"
 for i in range(6, 12):
-    forecast2 = forecast2 + "\n" + " " + hours[i] + "   " + newtemps[i] + "    " + newprecip[i]
-    
-forecast3 = "HOUR TEMP PRECIP"
-for i in range(12, 18):
-    forecast3 = forecast3 + "\n" + " " + hours[i] + "   " + newtemps[i] + "    " + newprecip[i]
+    forecast2 = forecast2 + "\n" + " " + hours[i] + "   " + newtemps[i]  + "   " + precip[i]
+
 
 #Then log into an email account to send the text messages to a phone
 #All phones are connected to an email with the following types of extensions:
@@ -67,7 +62,5 @@ server.login( '[youreamil]@gmail.com', '[yourpassword]' )
 server.sendmail( 'Weather', '5555555555@mms.att.net', forecast ) #use your number
 time.sleep(2)
 server.sendmail( 'Weather', '5555555555@mms.att.net', forecast2 )
-time.sleep(2)
-server.sendmail( 'Weather', '5555555555@mms.att.net', forecast3 )
 
 exit()
